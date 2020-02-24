34852ae0f99a8fdf618f7e17757ea68162ad6ebc
Isaac Renner <isaac@trunkplatform.com>
2020-02-24 11:39:39 +1100

    Working payment method id creation form

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,83 @@
-import React from "react"
+import React from 'react';
+import {useState} from 'react';
+import {loadStripe} from '@stripe/stripe-js';
+import {CardElement, Elements, useElements, useStripe} from '@stripe/react-stripe-js';
 
-class CreditCardForm extends React.component {
-  render(){
-    return(
-      <div>Hello world!</div>
-    )
-  }
-}
 
-export default () => CreditCardForm
+const CheckoutForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+  const [paymentMethodId, setPaymentMethodId] = useState(null);
+  
+  const handleSubmit = async (event) => {
+    // Block native form submission.
+    event.preventDefault();
+    
+    if (!stripe || !elements) {
+      // Stripe.js has not loaded yet. Make sure to disable
+      // form submission until Stripe.js has loaded.
+      return;
+    }
+    
+    // Get a reference to a mounted CardElement. Elements knows how
+    // to find your CardElement because there can only ever be one of
+    // each type of element.
+    const cardElement = elements.getElement(CardElement);
+    
+    // Use your card Element with other Stripe.js APIs
+    const {error, paymentMethod} = await stripe.createPaymentMethod({
+      type: 'card',
+      card: cardElement,
+    });
+    
+    if (error) {
+      console.log('[error]', error);
+    } else {
+      console.log('[PaymentMethod]', paymentMethod);
+      setPaymentMethodId(paymentMethod.id);
+    }
+  };
+  
+  return (
+    <>
+    <form onSubmit={handleSubmit}>
+      <CardElement
+        options={{
+          style: {
+            base: {
+              fontSize: '24px',
+              color: '#424770',
+              '::placeholder': {
+                color: '#aab7c4',
+              },
+            },
+            invalid: {
+              color: '#9e2146',
+            },
+          },
+        }}
+      />
+      <button type="submit" disabled={!stripe}>
+        Save Payment Method
+      </button>
+    </form>
+      <div>
+        {paymentMethodId}
+      </div>
+    </>
+  );
+};
+
+// Make sure to call `loadStripe` outside of a component’s render to avoid
+// recreating the `Stripe` object on every render.
+const stripePromise = loadStripe('pk_test_6pRNASCoBOKtIshFeQd4XMUh');
+
+const App = () => {
+  return (
+    <Elements stripe={stripePromise}>
+      <CheckoutForm />
+    </Elements>
+  );
+};
+
+export default App;
